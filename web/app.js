06425b0a64bee1bac0e87150270686a8c8b88dae
flumeware <flumeware@users.noreply.github.com>
2019-04-13 15:09:40 +0100

    adjusted web interface

--- web/app.js
+++ web/app.js
@@ -1,14 +1,62 @@
 var controls = [];
+const socket = new WebSocket('ws://192.168.1.122:8080');
+
+socket.addEventListener('message', function (event) {
+  msg_rx(event.data);
+});
+
+socket.onclose = function(event) {
+  console.log("WebSocket is closed now.");
+};
 
 //3 element data structure type=key=value1=value2
 //defined types are control to set a controls value, output to set a control to output mapping
 function msg_rx(message){
-  var parts = message.split("=");
+  var parts = message.split(":");
 
-  if(parts[0]=="control"){
-    update_table(parts);
+  if(parts[0]=="moved"){
+    update_control(parts);
   }
 }
+
+var lastmoved =""; 
+
+function update_control(parts){
+  var label = document.getElementById("movedcontrol");
+  var value = document.getElementById("movedcontrolvalue");
+  var cg =  document.getElementById("movedcontrolchannel");
+  var ag =  document.getElementById("movedcontrolaction");
+  var device = document.getElementById("movedcontroldevice");
+  var output = document.getElementById("movedcontroloutput");
+  label.innerHTML = parts[1];
+  value.innerHTML = parts[2];
+  output.innerHTML = parts[4];
+  device.innerHTML = parts[3];
+  //cg.innerHTML = parts[3];
+  //ag.innerHTML = parts[4];
+
+  lastmoved = parts[1];
+}
+
+var edit_control_name = "";
+
+function edit_mode(){
+  edit_control_name = lastmoved;
+  var output = document.getElementById("movedcontroloutput");
+  var edit_output = document.getElementById("edit-output");
+
+  document.getElementById("edit-output").value = document.getElementById("movedcontroloutput").innerHTML;
+   document.getElementById("edit-device").value = document.getElementById("movedcontroldevice").innerHTML;
+  document.getElementById("editcontrol").innerHTML = edit_control_name;
+}
+
+function set_control_output(){
+  var device = document.getElementById("edit-device");
+  var output = document.getElementById("edit-output");
+  socket.send("setControl:"+edit_control_name+":"+device.value+":"+output.value);
+}
+
+
 
 //processes messages which create controls which have 4 parts control=ctlname=value=definedoutput
 //for updates the definedoutput can be omitted
